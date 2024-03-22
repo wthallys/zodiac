2a9c8cb2a04f1ad0be553021a8b8bc89ba74bd2c
Wyddenberg Thallys <wthallys.02@gmail.com>
2024-03-22 01:38:30 -0300

    refactor: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './containers/Home';
 import SignSelectionPage from './containers/SignSelection';
 import ResultPage from './containers/Result/index';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/sign-selection/:firstSign" element={<SignSelectionPage />} />
-        <Route path="/result/:firstSign/:secondSign" element={<ResultPage />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sign-selection/:firstSign" element={<SignSelectionPage />} />
+          <Route path="/result/:firstSign/:secondSign" element={<ResultPage />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>,
 )
